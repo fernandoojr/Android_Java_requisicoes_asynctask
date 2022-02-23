797f2392d713e60f8e78fedca72c307a984f9602
fernandofojr <fernandofojr@gmail.com>
2022-02-23 19:54:02 -0300

    Initial commit

---
+++ app/src/main/java/com/cursoandroid/requisicoeshttp/MainActivity.java
@@ -0,0 +1,128 @@
+package com.cursoandroid.requisicoeshttp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnRecuperar;
+    private TextView txtResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtResultado = findViewById(R.id.txtResultado);
+        btnRecuperar = findViewById(R.id.btnRecuperar);
+
+        btnRecuperar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyTask task = new MyTask();
+                String urlApi = "https://blockchain.info/ticker";
+                String urlCep = "https://viacep.com.br/ws/14408140/json/";
+                task.execute(urlApi);
+            }
+        });
+    }
+
+    class MyTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String stringUrl = strings[0];
+            InputStream inputStream = null;
+            InputStreamReader inputStreamReader = null;
+            StringBuffer buffer = null;
+
+            try {
+                URL url = new URL(stringUrl);
+                HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
+
+                //Recupera os dados em Bytes
+                inputStream = conexao.getInputStream();
+
+                //InputStreamReader lê os dados em Bytes e decodifica para caracteres
+                inputStreamReader = new InputStreamReader(inputStream);
+
+                //Objeto utilizado para leitura dos caracteres do InputStreamReader
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+
+                String linha = "";
+                buffer = new StringBuffer();
+                while ((linha = reader.readLine()) != null){
+                    buffer.append(linha);
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return buffer.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            /*
+            String logradouro = null;
+            String cep = null;
+            String complemento = null;
+            String bairro = null;
+            String localidade = null;
+            String uf = null;
+             */
+
+            String objetoValor = null;
+            String valorMoeda = null;
+            String simbolo = null;
+            try {
+                /*
+                JSONObject jsonObject = new JSONObject(s);
+                logradouro = jsonObject.getString("logradouro");
+                cep = jsonObject.getString("cep");
+                complemento = jsonObject.getString("complemento");
+                bairro = jsonObject.getString("bairro");
+                localidade = jsonObject.getString("localidade");
+                uf = jsonObject.getString("uf");
+                 */
+
+                JSONObject jsonObject = new JSONObject(s);
+                objetoValor = jsonObject.getString("BRL");
+
+                JSONObject jsonObjectReal = new JSONObject(objetoValor);
+                valorMoeda = jsonObjectReal.getString("last");
+                simbolo = jsonObjectReal.getString("symbol");
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            //txtResultado.setText(s);
+            //txtResultado.setText(logradouro+" / "+cep+" / "+complemento+" / "+bairro+" / "+localidade+" / "+uf);
+            txtResultado.setText(simbolo + " " + valorMoeda);
+
+
+        }
+    }
+}
